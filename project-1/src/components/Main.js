611b1770df23900998b8df0a0fb41edf0906cfdc
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-08-26 16:52:54 +0500

    render basic appp

---
+++ project-1/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="">
+      <h1>Fact for React</h1>
+      <ul>
+        <li> Very Easy.</li>
+        <li>I enjoy it most.</li>
+        <li> Love it.</li>
+      </ul>
+    </main>
+  );
+}
---
+++ project-1/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar (){
+
+    return (
+      <nav className="nav">
+        <img src="./logo.png" alt="logo" />
+        <h3>Fact for React</h3>
+        <h4>Project 1</h4>
+      </nav>
+    );
+}
