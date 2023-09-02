44aebce76e7079fd6491f23e886a04825c4698e8
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-09-02 11:35:15 +0500

    props practice

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import NavBar from "./components/navbar.js";
 import Main from './components/main.js'
+import Joke from './components/joke.js'
 
 function App() {
   return (
     <>
       <NavBar />
       <Main />
+      <Joke
+        setup="I got my daughter a fridge for her birthday."
+        punchline="I can't wait to see her face light up when she opens it."
+      />
     </>
   );
 }
