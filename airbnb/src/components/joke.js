913ef0a4fd6ad6ae7d7e06fe712bb48e03de8a37
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-09-04 19:14:18 +0500

    some file get's updated

--- airbnb/src/components/joke.js
+++ airbnb/src/components/joke.js
@@ -4,10 +4,7 @@ import React from "react";
 export default function Joke (jokes){
 
     return (
-      <>
-        <h1>{jokes.setup}</h1>
-        <p>{jokes.punchline}</p>
-      </>
+
     );
 
 
