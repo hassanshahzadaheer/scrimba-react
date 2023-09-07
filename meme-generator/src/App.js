cffcf1c7416522a205c3813c909b3b27cb7d82cb
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-09-07 16:36:30 +0500

    update css for heder.js

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+function Pages() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
-export default App;
+
+export default function App() {
+  const jsxElement = <Pages/>
+
+  const root = document.getElementById("root");
+  ReactDOM.createRoot(root).render(jsxElement);
+}
---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+      <header className="header">
+        <img className="site--logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png" />
+        <h2 className="site--title">Meme Generator</h2>
+        <h4 className="site--projects">React Course - Project 3</h4>
+      </header>
+    );
+}
